12a962b01806a0e5c897d5e8ead2869c3fdcd5ce
Diego Mata <diegomata@hotmail.com>
2023-10-02 15:47:10 +0200

    producto agregado

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,7 @@ const inputProducto = document.querySelector('#inputProd');
 const inputCosto = document.querySelector('#inputCosto');
 const inputPrecio = document.querySelector('#inputPrecio');
 const list = document.querySelector('#cargado');
+const menuCompleto = document.querySelector('#completa');
 
 let listaProductos = [];
 let listaGuardada = localStorage.getItem("listaStorage");
@@ -15,7 +16,7 @@ if (listaGuardada) {
     listaProductos = JSON.parse(listaGuardada);
 }
 
-//Se crea una función constructora de nuevos productos y se delcara un array para contenerlos
+//Se crea una función constructora de nuevos productos
 class NuevoProducto {
     constructor (nombre, costo, precio, margen, rentabilidad) {
       this.nombre = nombre;
@@ -26,13 +27,7 @@ class NuevoProducto {
     }
   }
 
-  //función para el cálculo del margen y de la rentabilidad de los productos a ingresar
-function margenRentabilidad () {
-    margen = (precio - costo).toFixed(2);
-    rentabilidad = ((margen / costo) * 100).toFixed(2);
-}
-
-//Acciones que se disparan en el evento "submit" al hacer click en botón "Agregar"
+//Acciones que se disparan en el evento "submit" al hacer click en botón "Agregar" del formulario
 formIngreso.addEventListener('submit', e => {
     e.preventDefault();
     if (inputProducto.value == "") {
@@ -40,9 +35,13 @@ formIngreso.addEventListener('submit', e => {
     } else {
         nombre = inputProducto.value;
     }
+    
     costo = parseFloat(inputCosto.value);
-    precio = parseFloat(inputPrecio.value);  
+    validarCosto ();
+    precio = parseFloat(inputPrecio.value); 
+    validarPrecio ();
     margenRentabilidad ();
+
     const nuevoProducto = new NuevoProducto(nombre.toUpperCase(), costo, precio, margen, rentabilidad);
     listaProductos.push(nuevoProducto);
     localStorage.setItem("listaStorage", JSON.stringify(listaProductos));
@@ -53,8 +52,39 @@ formIngreso.addEventListener('submit', e => {
     inputPrecio.value = "";
 
 })
+// ---------   Funciones   ---------
 
-//Función para mostrar los datos que se van cargando en el formulario
+//función para el cálculo del margen y de la rentabilidad de los productos a ingresar
+function margenRentabilidad () {
+    margen = (precio - costo).toFixed(2);
+    rentabilidad = ((margen / costo) * 100).toFixed(2);
+}
+
+function validarCosto () {
+    while (isNaN(costo) || costo <= 0) {
+        alert ("Por favor ingrese un número válido")
+        costo = parseFloat(prompt ("Vuelva a ingresar el costo del producto: ")) 
+    }
+    return costo;
+}
+
+function validarPrecio () {
+    while (isNaN(precio) || precio <= 0) {
+        alert ("Por favor ingrese un número válido")
+        precio = parseFloat(prompt ("Vuelva a ingresar el precio del producto: ")) 
+    }
+    return precio;
+}
+
+function mostrarCarga() {
+    let mostrar = document.querySelector('#cargado');
+    list.innerHTML = "";
+    let lista = document.createElement("p")
+    lista.innerHTML = nombre + " -->  costo: $"+ costo + " precio: $" + precio;
+    mostrar.append(lista);
+    }
+
+/*
 function mostrarCarga() {
     const cargados = listaProductos.map((item) => {
         return {
@@ -63,20 +93,22 @@ function mostrarCarga() {
             precio: item.precio,
         }
     })
-    
     let mostrar = document.querySelector('#cargado');
+    list.innerHTML = "";
     for (const item of cargados) {
         let list = document.createElement("li")
-        list.innerHTML = item.nombre + " -->  costo: "+ item.costo + " precio: " + item.precio;
+        list.innerHTML = item.nombre + " -->  costo: $"+ item.costo + " precio: $" + item.precio;
         mostrar.append(list);
     }
 }
-//Función para mostrar la lista completa de productos cargados con el cálculo del margen y de la rentabilidad
+*/
+
 function mostrarMargenRentab() {
     let listaCompleta = document.querySelector('#completa');
+    menuCompleto.innerHTML = "";
     for (const items of listaProductos) {
         let menuCompleto = document.createElement("li");
-        menuCompleto.innerHTML = items.nombre + " -- margen: " + items.margen + " -- rentabilidad: " + items.rentabilidad;
+        menuCompleto.innerHTML = items.nombre + " -- margen: $" + items.margen + " -- rentabilidad: " + items.rentabilidad + "%";
         listaCompleta.append(menuCompleto);
     }
 }
